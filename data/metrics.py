8092eaedbd054f0c0f5e5c8c84dc5158a265d822
chen-yuxuan <yzchenyuxuan@gmail.com>
2022-02-25 17:09:44 +0100

    Add with and w/o versions for shannon-index

--- data/metrics.py
+++ data/metrics.py
@@ -4,61 +4,53 @@ import json
 import numpy as np
 
 
-def compute_shannon_index(count: Dict[Any, int], sum_exclude_negative: bool = True) -> float:
+def compute_shannon_index(
+    count: Dict[Any, int], with_negative: bool = True
+) -> float:
     count = list(count.values())
     count.sort()
     count = np.array(count)
+    if not with_negative:
+        count = count[:-1]
 
     proportion = count / sum(count)
     entropy_per_class = -proportion * np.log(proportion)
-    if sum_exclude_negative:
-        entropy_per_class = entropy_per_class[:-1]
     return float(sum(entropy_per_class))
 
 
-def compute_metrics(count_file: str, has_negative_class: bool=True) -> Dict[str, Any]:
-    dataset_name = "".join(count_file.split('_')[:-1]).replace('./', '')
+def compute_metrics(count_file: str) -> Dict[str, Any]:
+    dataset_name = "".join(count_file.split("_")[:-1]).replace("./", "")
+    has_negative_class = (
+        True if "tacred" in dataset_name or "semeval" in dataset_name else False
+    )
 
-    with open(count_file, 'r') as f:
+    with open(count_file, "r") as f:
         count: Dict[Any, int] = json.load(f)
-    #print(dataset_name, "\n", count)
 
     num_examples = sum(count.values())
-    if has_negative_class:
-        num_negative_examples = max(count.values())
-    else:
-        num_negative_examples = 0
-
-    # compute shannon_index
-    shannon_index = compute_shannon_index(count)
-    
-    return {
+    shannon_index_with_negative = compute_shannon_index(count, True)
+    report = {
         "dataset": dataset_name,
         "num_classes": len(count),
         "num_examples": num_examples,
-        "num_negative_examples": num_negative_examples,
-        "shannon_index": shannon_index,
+        "shannon_index_with_negative": shannon_index_with_negative,
     }
+
+    if has_negative_class:
+        report["num_negative_examples"] = max(count.values())
+        report["shannon_index_without_negative"] = compute_shannon_index(count, False)
+    else:
+        report["num_negative_examples"] = 0
+    return report
 
 
 if __name__ == "__main__":
-    print(compute_metrics("./tacred_count.json", True))
-    print(compute_metrics("./tacred_test_count.json", True))
+    print(compute_metrics("./tacred_count.json"))
+    print(compute_metrics("./tacred_test_count.json"))
 
-    print(compute_metrics("./semeval_count.json", True))
-    print(compute_metrics("./semeval_test_count.json", True))
+    print(compute_metrics("./semeval_count.json"))
+    print(compute_metrics("./semeval_test_count.json"))
 
-    print(compute_metrics("./chemprot_count.json", False))
+    print(compute_metrics("./chemprot_count.json"))
     # "SUBSTRATE_PRODUCT-OF" has no examples in test split
-    print(compute_metrics("./chemprot_test_count.json", False))
-
-
-
-""" 
-{'dataset': 'tacred', 'num_classes': 42, 'num_examples': 106264, 'num_negative_examples': 84491}
-{'dataset': 'tacredtest', 'num_classes': 42, 'num_examples': 15509, 'num_negative_examples': 12184}
-{'dataset': 'semeval', 'num_classes': 19, 'num_examples': 10717, 'num_negative_examples': 1864}
-{'dataset': 'semevaltest', 'num_classes': 19, 'num_examples': 2717, 'num_negative_examples': 454}
-{'dataset': 'chemprot', 'num_classes': 13, 'num_examples': 10065, 'num_negative_examples': 0}
-{'dataset': 'chemprottest', 'num_classes': 12, 'num_examples': 3469, 'num_negative_examples': 0}
-"""
+    print(compute_metrics("./chemprot_test_count.json"))
